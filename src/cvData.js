12bfd3ce4032213335c897f5f8333783ad4eaa04
Fatih Tezcan <fatihtezcan1071@gmail.com>
2024-07-05 00:05:06 +0300

    added https:// to the personal URL's

--- src/cvData.js
+++ src/cvData.js
@@ -3,9 +3,9 @@ export const cvData = {
   title: "Mathematical Engineer",
   phone: "+90 532 553 3019",
   email: "fatih.tezcan00@gmail.com",
-  linkedin: "linkedin.com/in/tezcanfatih",
-  github: "github.com/fatihmatik",
-  kaggle: "kaggle.com/fatihmatic",
+  linkedin: "https://linkedin.com/in/tezcanfatih",
+  github: "https://github.com/fatihmatik",
+  kaggle: "https://kaggle.com/fatihmatic",
   aboutText:
     "I’m a Mathematical Engineering graduate from Yıldız Technical University who is a motivated team player and eager to learn. Currently, I am actively seeking work experience to further develop my skills in the field of Data. Additionally, I am working on personal projects and completing courses related to my field. I'm interested in the fields of: ",
   highlightText:
